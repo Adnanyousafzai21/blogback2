a7e81cdd5ee2f99de3eada504c1e85d6b82b8ade
Adnanyousafzai21 <www.adnanrafiq@gmail.com>
2023-12-10 21:21:28 +0500

    last commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://blogfront2-t75k.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
